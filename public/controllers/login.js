69b227fdb58bc8d4e32850ac7d426a8c86111696
unknown <medaymen.mejri1@gmail.com>
2015-04-15 12:57:05 +0100

    test before ending

---
+++ public/controllers/login.js
@@ -0,0 +1,11 @@
+angular.module('MyApp')
+  .controller('LoginCtrl', function($scope, Auth) {
+    $scope.login = function() {
+      Auth.login({ email: $scope.email, password: $scope.password });
+    };
+    
+    $scope.googleLogin = function() {
+      Auth.googleLogin();
+    };
+    $scope.pageClass = 'fadeZoom';
+  });
